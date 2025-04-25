598a3f73c9257268f3bf7132895b33bd881e2603
Liam West <liamwest987@gmail.com>
2025-04-25 09:35:07 -0500

    added aero projects

--- main.js
+++ main.js
@@ -8,9 +8,9 @@ const projects = [
         image: '/photos/rng_dice.jpeg',
         description: 'This hardware RNG employs a 555 timer\'s capacitor decay and logic circuits to create randomized LED patterns, simulating dice rolls through analog signal fading and digital processing.<br>',
         longDescription: 'This project is a hardware-based Random Number Generator designed to mimic a dice roll using a 555 timer circuit. Entropy is introduced through the natural fading signal of a capacitor, which triggers sequential and combinational logic built from ICs and basic logic gates (AND, OR, NOT). The randomized outputs are displayed through LEDs, simulating dice values from 1 to 6. The design process involved circuit simulation using Multisim, developing and analyzing truth tables, and hands-on construction through soldering and prototyping. Through this project, I learned how to integrate analog randomness with digital logic systems and gained practical experience debugging mixed-signal circuits. If I were to do it again, I\'d add debouncing and output latching to stabilize the LED display, and explore more efficient logic mapping to reduce complexity. There\'s also room for improving randomness validation by applying basic statistical analysis to test output fairness.',
-        technologies: ['Multisim', 'Circuit Logic', 'Prototyping', 'Soldering'],
+        technologies: ['Multisim', 'Circuit Logic','Soldering'],
         pdf: '/pdfs/random_number_generator.pdf',
-        demo: 'https://drive.google.com/file/d/14m4PKFGFdnuxGtXrklP6yti_XJYTF-jA/view?usp=sharing'
+        //demo: 'https://drive.google.com/file/d/14m4PKFGFdnuxGtXrklP6yti_XJYTF-jA/view?usp=sharing'
     },
     {
         id: 2,
@@ -20,7 +20,7 @@ const projects = [
         image: '/photos/vote.jpg',
         description: 'A Majority Vote machine that uses logic gates to resolve voting controversies by ensuring majority decisions, prioritizing a tiebreaker, and validating reliability through truth tables and circuit designs.<br>',
         longDescription: 'I designed an electronic voting machine that uses specified logic gates (AND, OR, NAND, NOR, and inverters) to accurately determine majority decisions among a four-member board, with the president\'s vote used to break ties. The logic was developed and verified using truth tables, Karnaugh maps, and Boolean algebra simplification, and implemented using AOI, NAND-only, and NOR-only logic circuits. This project taught me how to translate real-world decision-making rules into reliable digital logic, and how to optimize those rules for efficiency and minimal gate usage. If I were to revisit the design, I\'d focus on modularizing the logic for easier scalability and explore implementing the system on a programmable logic device to improve flexibility and reduce physical complexity.',
-        technologies: ['MultiSim', 'Boolean Logic', 'Karnough Mapping', 'Prototyping'],
+        technologies: ['MultiSim', 'Boolean Logic', 'Karnough Mapping'],
         pdf: '/pdfs/majority_vote.pdf',
     },
     {
@@ -31,7 +31,7 @@ const projects = [
         image: '/photos/time.jpeg',
         description: 'This project converts binary switch inputs into a seven-segment display output for date storage, using logic simplification and circuit prototyping to minimize hardware complexity.<br>',
         longDescription: 'The Date of Birth project focuses on efficiently storing and displaying numerical dates using just three binary switches to control a single seven-segment display, minimizing hardware requirements. Logic design techniques—including truth tables, Karnaugh maps, and AOI, NAND, and NOR implementations—were used to decode binary inputs into the appropriate segmented outputs. The circuit was prototyped and tested using Tinkercad to ensure accurate display logic. Through this project, I deepened my understanding of binary-to-decimal conversion and display control using combinational logic. If I were to improve the design, I\'d aim to streamline the logic further using a dedicated decoder IC and expand the switch range to support double-digit outputs, enabling full date representation while maintaining minimal hardware.',
-        technologies: ['MultiSim', 'Boolean Logic', 'Tinkercad', 'Prototyping'],
+        technologies: ['MultiSim', 'Boolean Logic', 'Tinkercad'],
         pdf: '/pdfs/date_of_birth',
     },
     {
@@ -42,7 +42,7 @@ const projects = [
         image: '/photos/timer.jpg',
         description: 'I designed a digital timer circuit that uses logic components to display time on seven-segment displays.<br><br><br>',
         longDescription: 'In this project, I built a digital timer using 74LS93N and 74LS193N counter ICs along with flip-flop circuits to track and display seconds, minutes, and hours. The design operates sequentially, with each time unit triggering the next to maintain accurate timekeeping within set design constraints. Constructing this system helped me understand the coordination between asynchronous and synchronous counting, as well as the importance of timing and propagation delays in sequential logic. If I were to improve the project, I\'d focus on optimizing reset logic to better handle overflow conditions and explore integrating a crystal oscillator for more precise timing instead of relying solely on manual or unstable pulse inputs.',
-        technologies: ['Multisim', 'Synchronous Logic', 'Counters', 'Logic Design'],
+        technologies: ['Multisim', 'Synchronous Logic','Logic Design'],
         pdf: '/pdfs/digital_timer_report.pdf',
     },
     {
@@ -76,7 +76,7 @@ const projects = [
         image: '/photos/tensiletest.jpg',
         description: 'I built balsa wood trusses, tested them on an SSA Machine, and analyzed the resulting graph to understand key properties like modulus of toughness.<br>',
         longDescription: 'In this project, I constructed balsa wood trusses and tested their strength using an SSA Machine. I analyzed the resulting graph to identify important properties such as the modulus of elasticity and modulus of toughness, while also improving my attention to detail with units and numerical accuracy.',
-        technologies: ['SSA Machines', 'Property Testing', 'Rapid Development', 'Calculations'],
+        technologies: ['SSA Machines', 'Property Testing'],
         pdf: '/pdfs/tensile_test_ssa.pdf',
     },
     {
@@ -87,7 +87,7 @@ const projects = [
         image: '/photos/glider.png',
         description: 'This project involved designing, building, and testing a wooden glider to achieve long, stable flight using aerodynamic principles and Aery simulation software.<br>',
         longDescription: 'The objective was to create a glider that could fly far and straight by optimizing its design within strict material constraints and using Aery software to refine performance. We experimented with wing shape, center of gravity, and lift-to-drag ratios, then built and tested the glider through multiple launches. Through the process, I learned the importance of precision in both digital design and physical construction—and how small flaws like uneven sanding can make a big difference in real-world performance.',
-        technologies: ['Aerodynamics', 'Flight Testing', 'Structural Design'],
+        technologies: ['Flight Testing', 'Structural Design'],
         pdf: '/pdfs/glider_project.pdf',
     },
     {
@@ -109,8 +109,30 @@ const projects = [
         image: '/photos/rocket_launch.jpg',
         description: 'This project analyzed and compared rocket engine performance using simulations, thrust tests, and real launches to find the best engine for a small-scale launch.<br>',
         longDescription: 'The goal was to determine the ideal rocket engine for a constrained launch area by comparing thrust data, running OpenRocket simulations, and analyzing real-world performance. We tested multiple engines with a force sensor and matched simulation results to launch conditions, ultimately selecting the A8 engine for its lower thrust and safer range. I learned how simulation data and physical testing complement each other, and how even small differences, like wind resistance, can shift real outcomes from predicted ones.',
-        technologies: ['OpenRocket', 'Thrust Analysis', 'Data Collection', 'Simulation'],
+        technologies: ['OpenRocket', 'Thrust Analysis', 'Data Collection'],
         pdf: '/pdfs/rocket_analysis.pdf',
+    },
+    {
+        id: 11,
+        title: 'Spacecraft Propulsion',
+        category: 'PLTW Aerospace',
+        date: '2025-2-19',
+        image: '/photos/reaction_wheels.jpg',
+        description: 'This project explores how reaction wheels allow spacecraft to control their orientation using angular momentum rather than traditional fuel-based propulsion.<br>',
+        longDescription: 'The objective was to understand and explain how reaction wheels function and why they\’re crucial for fuel-efficient spacecraft attitude control. We researched their components, energy systems, and use cases, then analyzed how they apply Newton\'s Third Law to reorient a spacecraft without expelling mass. I learned how elegant and efficient non-thrust-based systems can be, especially in long-duration missions where conserving fuel is critical.',
+        technologies: ['Spacecraft Dynamics', 'Mechanical Principles'],
+        pdf: '/pdfs/spacecraft_propulsion.pdf',
+    },
+    {
+        id: 11,
+        title: 'Microgravity Presentation',
+        category: 'PLTW Aerospace',
+        date: '2025-3-12',
+        image: '/photos/microgravity.png',
+        description: 'This project explored how microgravity affects human vision, particularly the visual system\'s function and recovery during and after space travel.<br>',
+        longDescription: 'The objective was to investigate the effects of microgravity on the human visual system and understand how astronauts prepare for and recover from these changes. We researched fluid shifts in space, the impact on intraocular pressure and vision, and current countermeasures like vision training and pressure devices. I learned how spaceflight poses unique biomedical challenges and how critical vision health is to long-term human space exploration.',
+        technologies: ['Human Biology', 'Research', 'Presenting'],
+        pdf: '/pdfs/microgravity_presentation.pdf',
     }
 ];
 
